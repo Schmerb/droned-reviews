38984872a0ed17b05d31ace7103b602528be42fc
Schmerb <mike.schmerbeck@gmail.com>
2017-07-27 19:04:43 -0400

    rss feed in own route

--- mainRouter.js
+++ mainRouter.js
@@ -3,11 +3,60 @@ const bodyParser = require('body-parser');
 
 const router = express.Router();
 
+let jsonArr = [];
+
 // parser for requests
 router.use(bodyParser.json());
 
 router.get('/', (req, res) => {
     res.status(200).sendFile('./public/index.html');
+});
+
+router.get('/feed', (req, res) => {
+    const FeedParser = require('feedparser');
+    const request = require('request'); // for fetching the feed
+
+    let reqR = request('http://dronelife.com/feed/');
+    const feedparser = new FeedParser();
+    reqR.on('error', function (error) {
+        // handle any request errors
+    });
+
+    reqR.on('response', function (res) {
+        let stream = this; // `this` is `req`, which is a stream
+
+        if (res.statusCode !== 200) {
+        this.emit('error', new Error('Bad status code'));
+        }
+        else {
+        stream.pipe(feedparser);
+        
+        
+        }
+    });
+
+    feedparser.on('error', function (error) {
+        // always handle errors
+    });
+
+    feedparser.on('readable', function () {
+        // This is where the action is!
+        let stream = this; // `this` is `feedparser`, which is a stream
+        let meta = this.meta; // **NOTE** the "meta" is always available in the context of the feedparser instance
+        let item;
+
+        
+
+        while (item = stream.read()) {
+            console.log(item);
+            jsonArr.push(item);
+        }
+        // console.log(jsonArr.join(''));
+        // res.json({
+        //     "test": 123,
+        //     "json" : JSON.stringify(jsonArr.join(''))
+        // });
+    });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -7,12 +7,18 @@ const mongoose = require('mongoose');
 const mainRouter = require('./mainRouter');
 const {DATABASE_URL, PORT} = require('./config');
 
+const getFeed = require('./services/rssFeeds');
+
+
+
 const app = express();
 
 // log the http layer
 app.use(morgan('common'));
 // root folder for static files
 app.use(express.static('public'));
+
+app.use(getFeed);
 
 // router
 app.use('/', mainRouter);
@@ -21,6 +27,44 @@ app.use('*', (req, res) => {
     res.status(404).json({message: 'Not Found'});
 });
 
+////
+
+// const FeedParser = require('feedparser');
+//   const request = require('request'); // for fetching the feed
+
+//   let req = request('http://dronelife.com/feed/');
+//   const feedparser = new FeedParser();
+//   req.on('error', function (error) {
+//     // handle any request errors
+//   });
+
+//   req.on('response', function (res) {
+//     let stream = this; // `this` is `req`, which is a stream
+
+//     if (res.statusCode !== 200) {
+//       this.emit('error', new Error('Bad status code'));
+//     }
+//     else {
+//       stream.pipe(feedparser);
+//     }
+//   });
+
+//   feedparser.on('error', function (error) {
+//     // always handle errors
+//   });
+
+//   feedparser.on('readable', function () {
+//     // This is where the action is!
+//     let stream = this; // `this` is `feedparser`, which is a stream
+//     let meta = this.meta; // **NOTE** the "meta" is always available in the context of the feedparser instance
+//     let item;
+
+//     while (item = stream.read()) {
+//       console.log(item);
+//     }
+//   });
+
+////
 
 let server;
 
