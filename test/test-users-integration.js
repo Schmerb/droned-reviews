8e2cc5ae39de0bf9e53b2a7dbbb401d8a4af9e2f
Schmerb <mike.schmerbeck@gmail.com>
2017-08-23 19:43:06 -0400

    commented user tests out

--- test/test-users-integration.js
+++ test/test-users-integration.js
@@ -1,40 +1,40 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const faker = require('faker');
-const mongoose = require('mongoose');
+// const chai = require('chai');
+// const chaiHttp = require('chai-http');
+// const faker = require('faker');
+// const mongoose = require('mongoose');
 
-const should = chai.should();
+// const should = chai.should();
 
-const {User} = require('../models/user');
+// const {User} = require('../models/user');
 
-const {app, runServer, closeServer} = require('../server');
-const {TEST_DATABASE_URL} = require('../config/database');
+// const {app, runServer, closeServer} = require('../server');
+// const {TEST_DATABASE_URL} = require('../config/database');
 
-chai.use(chaiHttp);
+// chai.use(chaiHttp);
 
 
-describe('Droned /Comments API resource', function() {
+// describe('Droned /Comments API resource', function() {
 
-    // Runs once before all tests begin
-    // to start server
-    before(function() {
-        return runServer(TEST_DATABASE_URL);
-      });
+//     // Runs once before all tests begin
+//     // to start server
+//     before(function() {
+//         return runServer(TEST_DATABASE_URL);
+//       });
   
-      // Runs before each and every test to seed database
-      // with fresh data
-      beforeEach(function() {
-        return seedCommentData()
-      });
+//       // Runs before each and every test to seed database
+//       // with fresh data
+//       beforeEach(function() {
+//         return seedCommentData()
+//       });
   
-      // Runs after each and every test to clear database
-      afterEach(function() {
-        return tearDownDb();
-      });
+//       // Runs after each and every test to clear database
+//       afterEach(function() {
+//         return tearDownDb();
+//       });
   
-      // Runs once after all tetsts have finished to 
-      // shut down server
-      after(function() {
-        return closeServer();
-      });
-});
+//       // Runs once after all tetsts have finished to 
+//       // shut down server
+//       after(function() {
+//         return closeServer();
+//       });
+// });
