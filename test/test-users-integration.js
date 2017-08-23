3d10093b64f7e4c4dc4e5b2f5dcb3192da7ea970
Schmerb <mike.schmerbeck@gmail.com>
2017-08-23 19:39:33 -0400

    init commit

--- test/test-users-integration.js
+++ test/test-users-integration.js
@@ -11,3 +11,37 @@ const {app, runServer, closeServer} = require('../server');
 const {TEST_DATABASE_URL} = require('../config/database');
 
 chai.use(chaiHttp);
+
+
+
+describe('Droned /Comments API resource', function() {
+    
+        // Runs once before all tests begin
+        // to start server
+        before(function() {
+          return runServer(TEST_DATABASE_URL);
+        });
+    
+        // Runs before each and every test to seed database
+        // with fresh data
+        beforeEach(function() {
+          return seedCommentData()
+        });
+    
+        // Runs after each and every test to clear database
+        afterEach(function() {
+          return tearDownDb();
+        });
+    
+        // Runs once after all tetsts have finished to 
+        // shut down server
+        after(function() {
+          return closeServer();
+        });
+    
+        // GET -- READ
+        describe('GET endpoints', function() {
+
+        });
+        
+});
